0d8f55e34505f71eca2d5b11fd54cfdbcbad638f
Sungjoon Park <sungjoon512@gmail.com>
2020-11-29 17:13:37 +0900

    use Input type to simplify

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer, gql } = require('apollo-server');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 
-const CONSOLE_FLAG = true;
+const CONSOLE_FLAG = false;
 
 const typeDefs = gql`
   scalar Date
@@ -35,8 +35,21 @@ const typeDefs = gql`
     movie(id: ID): Movie
   }
 
+  input ActorInput {
+    id: ID
+  }
+
+  input MovieInput {
+    id: ID
+    title: String
+    releaseDate: Date
+    rating: Int
+    status: Status
+    actor: [ActorInput] # Subfield should end with Input
+  }
+
   type Mutation {
-    addMovie(title: String, releaseDate: Date, id: ID): [Movie]
+    addMovie(movie: MovieInput): [Movie]
   }
 `;
 
@@ -111,13 +124,14 @@ const resolvers = {
   },
 
   Mutation: {
-    addMovie: (obj, { id, title, releaseDate }, ctx, info) => {
+    addMovie: (obj, { movie }, ctx, info) => {
       if (CONSOLE_FLAG) {
         console.group('Mutation addMovie arguments');
         console.log(obj);
-        console.log(id);
-        console.log(title);
-        console.log(releaseDate);
+        console.log(movie);
+        // console.log(id);
+        // console.log(title);
+        // console.log(releaseDate);
         console.log(ctx);
         console.log(info);
         console.groupEnd();
@@ -126,11 +140,7 @@ const resolvers = {
       const newMoviesList = [
         ...movies,
         // new movie data
-        {
-          id,
-          title,
-          releaseDate,
-        },
+        movie,
       ];
       // Return data as expected in schema
       return newMoviesList;
